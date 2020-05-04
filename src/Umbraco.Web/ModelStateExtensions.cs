638055c729cc39c7635fd4d21a76e61d133fe106
Daniël Knippers <daniel.knippers@perplex.nl>
2020-05-04 17:03:51 +0200

    GetCulturesWithPropertyErrors: only return culture errors for non-segmented cultures

--- src/Umbraco.Web/ModelStateExtensions.cs
+++ src/Umbraco.Web/ModelStateExtensions.cs
@@ -88,8 +88,9 @@ namespace Umbraco.Web
         {
             //Add any culture specific errors here
             var cultureErrors = modelState.Keys
+                .Where(key => key.StartsWith("_Properties.")) //only choose _Properties errors
                 .Select(x => x.Split('.')) //split into parts
-                .Where(x => x.Length >= 3 && x[0] == "_Properties") //only choose _Properties errors
+                .Where(x => x.Length >= 4 && string.IsNullOrEmpty(x[3])) // Skip any segment validation errors
                 .Select(x => x[2]) //select the culture part
                 .Where(x => !x.IsNullOrWhiteSpace()) //if it has a value
                 //if it's marked "invariant" than return the default language, this is because we can only edit invariant properties on the default language
